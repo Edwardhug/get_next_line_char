2b8563385f0ad673aa663d1c381968598da5e3ca
Lucien Gabet <lgabet@z4r2p7.42lyon.fr>
2023-01-17 11:02:50 +0100

    new files _bonus

---
+++ get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgabet <lgabet@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/17 11:00:33 by lgabet            #+#    #+#             */
+/*   Updated: 2023/01/17 11:02:05 by lgabet           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#	define BUFFER_SIZE 10
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_fill_line(int fd, char *buff);
+char	*ft_strjoin(char *s1, char *s2);
+int		ft_strlen_buff(char *str);
+int		have_newline(char *str);
+void	ft_clear_buff(char *str);
+int		ft_strlen(char *str);
+
+# endif
